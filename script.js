cd8873d6b3cfac5f8745fb9364e52de9fb209de2
ABlasters <100992149+ABlasters@users.noreply.github.com>
2024-06-17 12:19:24 +0200

    modif

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://minecraft.net')}`)
-                    .then(response => {
-                      if (response.ok) return response.json()
-                      throw new Error('Network response was not ok.')
-                    })
-                    .then(data => console.log(data.contents));
+// with fetch
+fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('vscode.dev')}`)
+.then(response => {
+	if (response.ok) return response.json()
+	throw new Error('Network response was not ok.')
+})
+.then(data => console.log(data.contents));
+
+// with JQuery
+$.getJSON('http://api.allorigins.win/get?url=vscode.dev&callback=?', function (data) {
+	$('#output').html(data.contents);
+});
